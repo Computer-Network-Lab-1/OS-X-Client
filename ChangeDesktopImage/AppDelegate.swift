44aa967d031d6390023495deadeb0b94082617a1
cuty-lewiwi <wuyudong5426@163.com>
2015-03-21 10:12:15 +0800

    Initial Commit

---
+++ ChangeDesktopImage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ChangeDesktopImage
+//
+//  Created by cuty-lewiwi on 3/21/15.
+//  Copyright (c) 2015 cuty-lewiwi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
